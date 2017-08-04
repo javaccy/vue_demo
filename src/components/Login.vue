eb4b3338763e9fec17eeae3c64537cea5a2ae90f
javaccy <javaccy@gmail.com>
2017-08-04 22:10:31 +0800

    添加MuseUI

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <mu-appbar title="导航条">
+      <mu-icon-button icon="menu" slot="菜单"/>
+    </mu-appbar>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data () {
+      return {
+        msg: 'login'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
